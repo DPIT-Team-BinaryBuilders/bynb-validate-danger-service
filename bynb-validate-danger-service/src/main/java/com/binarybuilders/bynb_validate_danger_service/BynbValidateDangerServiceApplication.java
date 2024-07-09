c0b32660c903a84a477a4edfb031b794b5e95b16
Darius <darius.hodrea@gmail.com>
2024-07-09 12:59:49 +0300

    Initial commit

---
+++ bynb-validate-danger-service/src/main/java/com/binarybuilders/bynb_validate_danger_service/BynbValidateDangerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_validate_danger_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BynbValidateDangerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BynbValidateDangerServiceApplication.class, args);
+	}
+
+}
---
+++ bynb-validate-danger-service/src/test/java/com/binarybuilders/bynb_validate_danger_service/BynbValidateDangerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_validate_danger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BynbValidateDangerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
